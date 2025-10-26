93cbc17dd68be74483e004412ec89c71e4202706
om22shree <om22shree@gmail.com>
2025-10-26 13:58:32 +0530

    fix the codebase to pass CI tests

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -19,5 +19,5 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", errors.New("malformed authorization header")
 	}
 
-		return splitAuth[0], nil // Intentional bug to make tests fail
+		return splitAuth[1], nil
 }
